e491a6ee76d0e43433d8c27de00e912acfa1fb6f
ginuel73-dot <ginuel73@gmail.com>
2026-01-29 19:39:19 -0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-
 import pygame
 from game_logic import create_board, BLACK, WHITE, EMPTY
-
 
 pygame.init()
 
@@ -12,58 +10,50 @@ tela = pygame.display.set_mode((LARGURA_TELA, ALTURA_TELA))
 pygame.display.set_caption("Meu Reversi")
 
 
-TAMANHO_TABULEIRO = 10
+TAMANHO_TABULEIRO = 8  
 tabuleiro = create_board()  
 
-
 tamanho_celula = ALTURA_TELA // TAMANHO_TABULEIRO
-
-
 deslocamento_x = (LARGURA_TELA - (tamanho_celula * TAMANHO_TABULEIRO)) // 2
-deslocamento_y = 0  # topo da tela
-
+deslocamento_y = 0  
 
 jogo_ativo = True
 while jogo_ativo:
-  
+    # Preenche fundo verde
     tela.fill((0, 150, 0))
 
-        for evento in pygame.event.get():
+   
+    for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             jogo_ativo = False
 
-    
+
     for linha in range(TAMANHO_TABULEIRO):
         for coluna in range(TAMANHO_TABULEIRO):
-           
             x = deslocamento_x + coluna * tamanho_celula
             y = deslocamento_y + linha * tamanho_celula
 
 
             pygame.draw.rect(tela, (0, 0, 0), (x, y, tamanho_celula, tamanho_celula), 2)
 
-                        if tabuleiro[linha][coluna] == BLACK:
+
+            if tabuleiro[linha][coluna] == BLACK:
                 pygame.draw.circle(
                     tela,
-                    (30, 30, 30),  # cor preta levemente cinza
-                    (x + int(tamanho_celula * 0.5) + 1, y + int(tamanho_celula * 0.5) + 1),
+                    (30, 30, 30),  # preto
+                    (x + tamanho_celula // 2, y + tamanho_celula // 2),
                     int(tamanho_celula * 0.45)
                 )
 
-           
-            if tabuleiro[linha][coluna] == WHITE:
+            elif tabuleiro[linha][coluna] == WHITE:
                 pygame.draw.circle(
                     tela,
-                    (230, 230, 230), 
-                    (x + int(tamanho_celula * 0.5) + 1, y + int(tamanho_celula * 0.5) + 1),
+                    (230, 230, 230),  # branco
+                    (x + tamanho_celula // 2, y + tamanho_celula // 2),
                     int(tamanho_celula * 0.45)
                 )
 
    
     pygame.display.flip()
 
-
 pygame.quit()
-
-
-
